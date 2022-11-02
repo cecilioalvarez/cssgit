79550b71217346020aed1bcf37ee07a06f1c3cfa
cecilioAlvarez <cmac@arquitecturajava.com>
2022-11-02 11:52:59 +0100

    cupon

---
+++ javascript/006.js
@@ -0,0 +1,9 @@
+var gasto=100;
+console.log("gasto 1")
+console.log(gasto);
+console.log("gasto 2");
+gasto=200;
+console.log(gasto);
+console.log("gasto3");
+console.log(gasto *1.21);
+//https://cursos.arquitecturajava.com/p/curso-javascript?coupon_code=AYUNTAMIENTO2022
