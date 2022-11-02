56bf18c3bf0a88d42c81180f88195004e2df0fd2
cecilioAlvarez <cmac@arquitecturajava.com>
2022-11-02 13:21:08 +0100

    if complementario

---
+++ javascript/012.js
@@ -0,0 +1,8 @@
+var importe =80;
+
+if (importe<50) {
+
+    console.log(importe*0.90);
+}else {
+    console.log(importe*0.80);
+}
---
+++ javascript/013.js
@@ -0,0 +1,5 @@
+var gasto1=parseInt(process.argv[2]);
+var gasto2=parseInt(process.argv[3]);
+var gasto3=parseInt(process.argv[4]);
+var total= gasto1+gasto2+gasto3;
+console.log(total*1.21);
