fcc58621ab5e50683462a3d47f125e1be9e4bcf0
cecilioAlvarez <cmac@arquitecturajava.com>
2022-11-03 09:28:56 +0100

    if anidado

---
+++ javascript/015.js
@@ -0,0 +1,15 @@
+var importe=100;
+
+if(importe>0 && importe <100) {
+
+
+      if (importe>0 && importe<50) {
+
+        console.log("es barato");
+      }else {
+        console.log("es caro");
+      }
+
+}else {
+  console.log("es carisimo");
+}
