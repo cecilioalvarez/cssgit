baf5be2bc97b74b1df7d7a649cf876277ef1eb85
cecilioAlvarez <cmac@arquitecturajava.com>
2022-11-03 13:34:16 +0100

    iteracion compleja

--- javascript/026.js
+++ javascript/026.js
@@ -1,8 +1,13 @@
 //acumulador
-var linea="";
-for (var i=0;i<=10;i=i+1) {
-  linea=linea+","+i;
+var linea = "";
+for (var i = 0; i <= 10; i = i + 1) {
+  if (i == 0) {
+    linea = linea + "(" + i;
+  }else if (i==10) {
+    linea = linea + "," + i+ ")";
+  }
+   else {
+    linea = linea + "," + i;
+  }
 }
 console.log(linea);
-
-
