5d5f74a8c056e0aac39d7c503a8cc5c3367b0bea
cecilioAlvarez <cmac@arquitecturajava.com>
2022-11-16 12:05:46 +0100

    suma

---
+++ php/basico/suma.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <?php
+        $numero1=$_GET["numero1"];
+        $numero2=$_GET["numero2"];
+        $suma=$numero1+$numero2;
+        echo $suma;
+        
+</body>
+</html>
