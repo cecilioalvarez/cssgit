87e25ec4e5f806918632017d146bb50c275ad4a9
cecilioAlvarez <cmac@arquitecturajava.com>
2022-11-16 13:34:48 +0100

    tabla multiplicar

---
+++ php/basico/tablamultiplicar.php
@@ -0,0 +1,18 @@
+<html>
+
+<body>
+    <table border="1px solid black">
+        <tr>
+            <th>multiplicacion</th>
+        </tr>
+        <?php for($i=0;$i<10;$i++){ ?>
+
+        <tr>
+            <td><?=($i*7)?></td>
+        </tr>
+
+        <?php }?>
+    </table>
+</body>
+
+</html>
